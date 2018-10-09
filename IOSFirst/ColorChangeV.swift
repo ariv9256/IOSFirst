5d1890751f12f605edb07459e03e2ae6b336ff3c
Angela Rivas <snowcats001@gmail.com>
2018-10-09 12:20:49 -0600

    Added color group in Swift

--- IOSFirst/ColorChangeV.swift
+++ IOSFirst/ColorChangeV.swift
@@ -10,14 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    public override func ColorChangeViewDidLoad() -> Void {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
---
+++ IOSFirst/Model/ColorTool.swift
@@ -0,0 +1,15 @@
+//
+//  ColorTool.swift
+//  IOSFirst
+//
+//  Created by Rivas, Angela on 10/9/18.
+//  Copyright © 2018 CTEC CSP. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class ColorTool
+{
+    
+}
