ed78965b657765da43fdba9ce6f6e76198041b63
Angela Rivas <snowcats001@gmail.com>
2018-10-09 11:56:17 -0600

    created gitignore and project, added xcuserstate to gitignore

---
+++ IOSFirst/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOSFirst
+//
+//  Created by Rivas, Angela on 10/9/18.
+//  Copyright © 2018 CTEC CSP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
